88bd7ccf17d0dc8f6acade0c84ec5761fe76542c
reem stamker <reem.stamker@gmail.com>
2022-04-23 13:04:12 +0300

    first
    f

---
+++ client/src/components/Form/Form.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <h2>Form</h2>
+  )
+}
+
+export default Form
---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Post from "./Post/Post"
+
+const Posts = () => {
+  return (
+    <>
+    <h2>Posts</h2>
+      <Post />
+      <Post />
+    </>
+  )
+}
+
+export default Posts
